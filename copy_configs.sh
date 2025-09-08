55e8515ebdaf574db45e9fc5474c13f93686b842
antonybstack <antonyblyakher@gmail.com>
2025-09-07 21:10:09 -0700

    Add settings.json for VS Code configuration and update copy_configs.sh

--- copy_configs.sh
+++ copy_configs.sh
@@ -3,6 +3,7 @@
 # Copy vim configuration files from home directory to current directory
 cp ~/.ideavimrc .ideavimrc
 cp ~/.vsvimrc .vsvimrc
+cp ~/Library/Application\ Support/Code/User/settings.json settings.json
 cp ~/.vimrc .vimrc
 
-echo "Copied .ideavimrc, .vsvimrc, and .vimrc from home directory to current directory"
+echo "Copied .ideavimrc, .vsvimrc, .vscvimrc, and .vimrc from home directory to current directory"
