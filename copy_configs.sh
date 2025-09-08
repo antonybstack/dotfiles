632dac480cb3c1e34403386590a4f92173532fb5
antonybstack <antonyblyakher@gmail.com>
2025-09-07 20:49:35 -0700

    Add .vsvimrc and copy_configs.sh for Vim configuration management

--- copy_configs.sh
+++ copy_configs.sh
@@ -2,6 +2,7 @@
 
 # Copy vim configuration files from home directory to current directory
 cp ~/.ideavimrc .ideavimrc
+cp ~/.vsvimrc .vsvimrc
 cp ~/.vimrc .vimrc
 
-echo "Copied .ideavimrc and .vimrc from home directory to current directory"
+echo "Copied .ideavimrc, .vsvimrc, and .vimrc from home directory to current directory"
